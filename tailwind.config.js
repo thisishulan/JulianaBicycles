30efed2e82cff803548611af3b8f06d4d3f9689a
thisishulan <bhulan1224@gmail.com>
2021-05-07 01:09:34 +0900

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+      height:{
+        '10vh':"10vh",
+        '20vh':"20vh",
+        '30vh':"30vh",
+        '40vh':"40vh",
+        '50vh':"50vh",
+        '60vh':"60vh",
+        '70vh':"70vh",
+        '80vh':"80vh",
+        '90vh':"90vh",
+        '100vh':"100vh"
+      },
+
+      inset:{
+        '1/2':"50%"
+      },
+
+      fontFamily:{
+        'main':['Montserrat']
+      },
+
+      fontSize: {
+        xs: ['0.75rem', { lineHeight: '1rem' }],
+        sm: ['0.875rem', { lineHeight: '1.25rem' }],
+        base: ['1rem', { lineHeight: '1.5rem' }],
+        lg: ['1.125rem', { lineHeight: '1.75rem' }],
+        xl: ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        '5xl': ['3rem', { lineHeight: '1' }],
+        '6xl': ['3.75rem', { lineHeight: '1' }],
+        '7xl': ['4.5rem', { lineHeight: '1' }],
+        '8xl': ['6rem', { lineHeight: '1' }],
+        '9xl': ['8rem', { lineHeight: '1' }],
+      },
+
+      colors:{
+        dark:"#181818",
+        primary:"#d29e0e",
+        secondary:"#e5001c",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
